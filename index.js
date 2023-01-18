83a629fa833d0091272fa9158903a535f0e190bc
Kaiya Takahashi <yaikashi@icloud.com>
2023-01-17 19:59:52 -0800

    add gitignore file

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
+const path = require('path');
+// Set up dotenv
+require('dotenv').config();
 
 // Configure database
 const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    port: 5432,
-    database: "dev_time_manager_db",
-})
+const devConfig = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+}
+const proConfig = {
+    connectionString: process.env.DATABASE_URL // heroku addons
+}
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? proConfig: devConfig
+)
 
 const app = express();
 
@@ -25,6 +34,12 @@ app.use(cors({
     credentials: true,
 }))
 
+if (process.env.NODE_ENV === "production") {
+    // server static content
+    // npm run build
+    app.use(express.static(path.join(__dirname, "client/buid")));
+}
+
 // Set up localStorage
 var LocalStorage = require('node-localstorage').LocalStorage,
 localStorage = new LocalStorage('./scratch');
@@ -33,9 +48,6 @@ localStorage = new LocalStorage('./scratch');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
-
-// Set up dotenv
-require('dotenv').config();
 
 // Set up google api
 const { google } = require('googleapis');
@@ -182,5 +194,5 @@ app.post('/create_event', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("running server");
+    console.log("running server on", PORT);
 })
