51a3edaaf83d9f7e2c56643f0a19c3dd2831804f
Kaiya Takahashi <yaikashi@icloud.com>
2023-01-19 23:58:56 -0800

    fix database config code

--- db/db.js
+++ db/db.js
@@ -7,11 +7,7 @@ const devConfig = {
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
 }
-const proConfig = {
-    connectionString: process.env.DATABASE_URL // heroku addons
-}
-const pool = new Pool(
-    process.env.NODE_ENV === "production" ? proConfig: devConfig
-)
+
+const pool = new Pool(devConfig)
 
 module.exports = pool;
