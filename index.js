33667b10653d21efef54062a7fa2fa78af1a2398
Kaiya Takahashi <yaikashi@icloud.com>
2023-02-04 22:49:47 -0800

    add cors configuration code by using app.use

--- index.js
+++ index.js
@@ -18,8 +18,26 @@ app.use(cors({
     origin: ['https://dev-time-manager-api.onrender.com', 'https://dancing-taiyaki-47a928.netlify.app', 'https://dancing-taiyaki-47a928.netlify.app/' ,'http://localhost:3000', 'https://dev-time-manager-api.onrender.com:'],
     methods: ["GET", "POST", "DELETE", "UPDATE"],
     credentials: true,
-    allowedHeaders: "*",
 }))
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://dancing-taiyaki-47a928.netlify.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(express.static('build'))
 // Set up Express
 app.use(bodyParser.urlencoded({ extended: true }));
