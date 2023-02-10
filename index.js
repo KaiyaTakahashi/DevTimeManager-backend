05b21e655a1412a42655d82b0cc770a0f00b7b49
Kaiya Takahashi <yaikashi@icloud.com>
2023-02-10 11:26:15 -0800

    use wildcard and delete access control allow credentials

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ require('dotenv').config();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://dancing-taiyaki-47a928.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
