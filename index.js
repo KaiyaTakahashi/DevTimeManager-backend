bb992cf46aaebdb749b99704263c3504b4b671a3
Kaiya Takahashi <yaikashi@icloud.com>
2023-02-11 16:49:55 -0800

    delete unnecessary code

--- index.js
+++ index.js
@@ -13,32 +13,20 @@ const userRoutes = require('./routes/userRoutes.js');
 // Set up dotenv
 require('dotenv').config();
 
-// middleware
-// app.use(cors({
-//     origin: ['https://dev-time-manager-api.onrender.com', 'https://dancing-taiyaki-47a928.netlify.app', 'https://dancing-taiyaki-47a928.netlify.app/' ,'http://localhost:3000', 'https://dev-time-manager-api.onrender.com:'],
-//     methods: ["GET", "POST", "DELETE", "UPDATE"],
-//     credentials: true,
-// }))
 app.use(function (req, res, next) {
-
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://dancing-taiyaki-47a928.netlify.app');
-
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     // Pass to next layer of middleware
     next();
 });
 
-app.use(express.static('build'))
 // Set up Express
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
